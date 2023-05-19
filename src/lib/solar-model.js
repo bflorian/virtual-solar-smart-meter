66fc16e4de86dfd995ca5bdbdbe055f3c2f6ae3d
Bob Florian <bob@smartthings.com>
2023-05-19 17:27:17 -0400

    Updated solar model and device profiles

--- src/lib/solar-model.js
+++ src/lib/solar-model.js
@@ -1,20 +1,10 @@
+const hashSum = require('hash-sum')
 const {findTimeZone, getZonedTime} = require('timezone-support')
-
-// Solar data indexed by 15 minute local time since midnight intervals
-const solarData = [
-	0,    0,    0,    0,    0,    0,    0,    0,
-	0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-	0,    0,    0,    0,   26,  107,  208,  345,  513,  734,  987, 1266,
-	1531, 1849, 2259, 2662, 3124, 3562, 4023, 4564, 5044, 5486, 6022, 6311,
-	6636, 6880, 6893, 6599, 7348, 6427, 7233, 7630, 6660, 6456, 7405, 6278,
-	6534, 6805, 6925, 6683, 6283, 5770, 5206, 5527, 4785, 4786, 3055, 3897,
-	3199, 2227, 2370, 1579, 1291,  964,  698,  508,  368,  257,  159,   83,
-	2,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-	0,    0,    0,    0,
-];
+const solarData = require('./solar-data')
 
 // Maximum value in the solar data
-const maximum = solarData.reduce((max, it) => {max = Math.max(max, it); return max},0)
+const maximum = solarData.flat()
+	.reduce((max, it) => {max = Math.max(max, it); return max},0)
 
 /**
  * Calculate the solar power production for the given date and time zone.
@@ -24,14 +14,15 @@ const maximum = solarData.reduce((max, it) => {max = Math.max(max, it); return m
  * @returns {number}
  */
 module.exports.solarPower = function(date, timeZone, maxPower) {
-
+	const dayIndex = parseInt(hashSum(date.toDateString()), 16) % solarData.length
+	const dayData = solarData[dayIndex]
 	const t = getZonedTime(date, findTimeZone(timeZone))
 	const offset = (t.hours * 3600) + (t.minutes * 60) + t.seconds
 	const index1 = Math.floor(offset / 900);
-	let power = solarData[solarData.length - 1];
-	if (index1 < solarData.length) {
-		const value1 = solarData[index1];
-		const value2 = index1 + 1 < solarData.length ? solarData[index1 + 1] : value1;
+	let power = dayData[dayData.length - 1];
+	if (index1 < dayData.length) {
+		const value1 = dayData[index1];
+		const value2 = index1 + 1 < dayData.length ? dayData[index1 + 1] : value1;
 		power = Math.round((value1 + value2) / 2);
 	}
 	return Math.round(maxPower * power / maximum)
